15aec3e68601710f6aba2af88c470e8d7b14bfef
danny <daniel-oy-mis@hotmail.com>
2018-12-09 16:57:44 -0600

    Ventana de login listo

--- app/src/main/java/com/example/danny/apprepositorio/LoginActivity.java
+++ app/src/main/java/com/example/danny/apprepositorio/LoginActivity.java
@@ -1,7 +1,9 @@
 package com.example.danny.apprepositorio;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         //comentario
+    }
+    public void iniciar(View view){
+        Intent i = new Intent(getApplicationContext(),MainActivity.class);
+        startActivity(i);
     }
 }
