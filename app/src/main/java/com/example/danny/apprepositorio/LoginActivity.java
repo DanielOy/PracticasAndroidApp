a704d288cd94a19f39f6e5a5fefb90acf74b1098
danny <daniel-oy-mis@hotmail.com>
2018-12-10 17:37:05 -0600

    consulta a la bd si existe el usuario

--- app/src/main/java/com/example/danny/apprepositorio/LoginActivity.java
+++ app/src/main/java/com/example/danny/apprepositorio/LoginActivity.java
@@ -1,26 +1,60 @@
 package com.example.danny.apprepositorio;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
+
+import com.example.danny.apprepositorio.utilidades.Utilidades;
 
 public class LoginActivity extends AppCompatActivity {
 
+    ConexionSQLiteHelper conn;
+    EditText user, pass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        ConexionSQLiteHelper conn = new ConexionSQLiteHelper(getApplicationContext(),"bd_app",null,1);
+        conn = new ConexionSQLiteHelper(getApplicationContext(),"bd_app",null,1);
         Toast.makeText(this, "Base de datos creada", Toast.LENGTH_SHORT).show();
+        user = (EditText)findViewById(R.id.edtUser);
+        pass = (EditText)findViewById(R.id.edtPass);
     }
     public void Registarse (View view){
         Intent i = new Intent(getApplicationContext(),RegistroActivity.class);
         startActivity(i);
     }
     public void Entrar (View view){
-        Intent i = new Intent(getApplicationContext(),HomeActivity.class);
-        startActivity(i);
+        consultar();
+    }
+
+    public void consultar(){
+        try {
+        SQLiteDatabase db = conn.getReadableDatabase();
+        String[] parametros = {user.getText().toString(),pass.getText().toString()};
+        String[] campos = {Utilidades.CAMPO_NOMBRE,Utilidades.CAMPO_CONTRASENIA};
+
+            String x, y;
+            Cursor cursor = db.query(Utilidades.TABLA_USUARIOS, campos, Utilidades.CAMPO_NOMBRE + "=? AND " + Utilidades.CAMPO_CONTRASENIA+"=?", parametros, null, null, null);
+            cursor.moveToFirst();
+            x=cursor.getString(0);
+            y=cursor.getString(1);
+
+            if (x.equals(user.getText().toString())&&y.equals(pass.getText().toString()))
+            {
+                //Abrimos la ventana
+                Intent i = new Intent(getApplicationContext(),HomeActivity.class);
+                startActivity(i);
+            }
+
+            cursor.close();
+        }catch (Exception e)
+        {
+            Toast.makeText(this, "El usuario no existe.", Toast.LENGTH_SHORT).show();
+        }
     }
 }
