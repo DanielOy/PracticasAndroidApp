b458c8faa86b2a2dc66f3680e3083adc14588ed4
danny <daniel-oy-mis@hotmail.com>
2018-12-10 18:47:08 -0600

    Creacion de ventana de configuraciones 2

--- app/src/main/java/com/example/danny/apprepositorio/SettingsActivity.java
+++ app/src/main/java/com/example/danny/apprepositorio/SettingsActivity.java
@@ -18,6 +18,7 @@ import android.preference.PreferenceManager;
 import android.preference.RingtonePreference;
 import android.text.TextUtils;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import java.util.List;
 
