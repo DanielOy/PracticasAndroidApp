c06ce12a9d9386bb6e79eb735a6cf28ba4593a49
ccwfire <sergio_ramirez_fernandez@hotmail.com>
2018-12-10 18:25:50 -0600

    Añadiendo Camara

--- app/src/main/java/com/example/danny/apprepositorio/PerfilActivity.java
+++ app/src/main/java/com/example/danny/apprepositorio/PerfilActivity.java
@@ -2,12 +2,19 @@ package com.example.danny.apprepositorio;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class PerfilActivity extends AppCompatActivity {
 
+    ImageButton botonCargar;
+    String path;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil);
     }
+
+
 }
