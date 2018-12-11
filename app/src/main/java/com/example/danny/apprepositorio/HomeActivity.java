8fe18d296998484d469e3e90eec83c62521f3bab
danny <daniel-oy-mis@hotmail.com>
2018-12-10 23:51:18 -0600

    Inserccion de iconos, cambios  a los botones

--- app/src/main/java/com/example/danny/apprepositorio/HomeActivity.java
+++ app/src/main/java/com/example/danny/apprepositorio/HomeActivity.java
@@ -25,14 +25,14 @@ public class HomeActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -96,7 +96,8 @@ public class HomeActivity extends AppCompatActivity
             startActivity(mapa);
 
         } else if (id == R.id.nav_send) {
-
+            Intent file = new Intent(getApplicationContext(),UploadActivity.class);
+            startActivity(file);
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
--- app/src/main/java/com/example/danny/apprepositorio/UploadActivity.java
+++ app/src/main/java/com/example/danny/apprepositorio/UploadActivity.java
@@ -1,7 +1,9 @@
 package com.example.danny.apprepositorio;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class UploadActivity extends AppCompatActivity {
 
@@ -9,5 +11,18 @@ public class UploadActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload);
+    }
+
+    private static final int READ_REQUEST_CODE=42;
+    public void demo(View view)
+    {
+        performFileSearch();
+    }
+    public void performFileSearch(){
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("*/*");
+        startActivityForResult(intent,READ_REQUEST_CODE);
+
     }
 }
