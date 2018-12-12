7edc6f9a8901ed0cbd7e403805fdb5091f0809b0
danny <daniel-oy-mis@hotmail.com>
2018-12-11 19:54:43 -0600

    Mejora del menu de inicio

--- app/src/main/java/com/example/danny/apprepositorio/ForoActivity.java
+++ app/src/main/java/com/example/danny/apprepositorio/ForoActivity.java
@@ -1,13 +1,46 @@
 package com.example.danny.apprepositorio;
 
+import android.net.Uri;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class ForoActivity extends AppCompatActivity {
+public class ForoActivity extends AppCompatActivity
+        implements ForoAportacionesFragment.OnFragmentInteractionListener,
+        ForoPrincipalFragment.OnFragmentInteractionListener
+{
 
+    ForoPrincipalFragment fragmentPrincipal;
+    ForoAportacionesFragment fragmetAportaciones;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_foro);
+
+        fragmentPrincipal=new ForoPrincipalFragment();
+        fragmetAportaciones = new ForoAportacionesFragment();
+
+        getSupportFragmentManager().beginTransaction().add(R.id.contenedorrFragment,fragmentPrincipal).commit();
+
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+
+    public void onclickForo(View view) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        switch (view.getId())
+        {
+            case R.id.btnPrincipal:
+                transaction.replace(R.id.contenedorrFragment,fragmentPrincipal);
+                break;
+            case R.id.btnAportaciones:
+                transaction.replace(R.id.contenedorrFragment,fragmetAportaciones);
+                break;
+        }
+        transaction.commit();
     }
 }
